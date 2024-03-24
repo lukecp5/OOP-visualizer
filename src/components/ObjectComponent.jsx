201f09588cf499d66384a1210119047dfd0ceb1e
Luke Poirrier <lukepoirrier5@gmail.com>
2024-03-23 23:03:40 -0400

    Add ObjectComponent to project to represent Java Objects

---
+++ src/components/ObjectComponent.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class ObjectComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        // Add your constructor logic here
+        this.name = "John Doe";
+    }
+
+    render() {
+        // Add your render logic here
+        return (
+            <div>
+                {
+                    <h1>{this.name}</h1>
+                }
+            </div>
+        );
+    }
+}
+
+export default ObjectComponent;
