c3a113ba239c31dbb24810999cdb9ace0a39cf04
Luke Poirrier <lukepoirrier5@gmail.com>
2024-03-23 23:40:52 -0400

    Add @babel/plugin-proposal-private-property-in-object dependency and update ClassComponent.jsx

--- src/components/ClassComponent.jsx
+++ src/components/ClassComponent.jsx
@@ -1,34 +1,37 @@
 /* eslint-disable no-useless-constructor */
 // ClassComponent.js
-import React from 'react';
+import React from "react";
 
 class ClassComponent extends React.Component {
-    constructor(props, name = "", type = "", properties = []) {
-        super(props);
+  constructor(props, name = "", type = "", properties = []) {
+    super(props);
 
-        // Add your constructor logic here
-        this.name = name;
-        this.type = type;
-        this.properties = properties;
-    }
-    
-    render() {
-        return(<div className="class">
-            {
-        /*  */}
-            <h2>Class</h2>
-            <p>Properties:</p>
-            <ul>
-                <li>Property 1</li>
-                <li>Property 2</li>
-            </ul>
-            <p>Methods:</p>
-            <ul>
-                <li>Method 1()</li>
-                <li>Method 2()</li>
-            </ul>
-        </div>
-    )};
-};
+    // Add your constructor logic here
+    this.name = name;
+    this.type = type;
+    this.properties = properties;
+  }
+
+  render() {
+    return (
+      <div className="class">
+        {/* Check if the name is not empty. If not, display the name
+         */}
+        {this.name && <h1>{this.name}</h1>}
+        <h2>{this.type && this.type}</h2>
+        <p>Properties:</p>
+        <ul>
+          <li>Property 1</li>
+          <li>Property 2</li>
+        </ul>
+        <p>Methods:</p>
+        <ul>
+          <li>Method 1()</li>
+          <li>Method 2()</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default ClassComponent;
